5a2cd4e59fad521c0ecc009e3042323de5531c92
jackcsheehan <jackcsheehan@users.noreply.github.com>
2021-05-04 09:53:10 -0500

    Added trim and split helper functions

--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,5 +1,5 @@
 #include "compiler.h"
-
+// TODO: work on parsing parseState -> .split isn't a legit function, write private function to do that
 // Takes strings of source file path
 Compiler::Compiler(string f) {
 	src.open(f);
@@ -10,15 +10,55 @@ Compiler::~Compiler() {
 	src.close();
 }
 
+// Trims whitespace from either end of given string
+string Compiler::trim(string str) {
+	// Indexes of string with starting and ending whitespace omitted
+	int start = 0;
+	int end = str.size() - 1;
+
+	// Find start of non-whitespace
+	while (start < str.size() && isspace(str.at(start))) ++start;
+
+	// Find end of non-whitespace
+	while (end > 0 && isspace(str.at(end))) --end;
+
+	return str.substr(start, end + 1);
+}
+
+// Splits given str at given delim. Puts split strings into given string array
+void Compiler::split(string str, char delim, string* arr) {
+	// Current split being created
+	string currentSplit = "";
+
+	// Current index being written to in string array
+	int i = 0;
+
+	// Iterate through each char in given str
+	for (char const &c : str) {
+		// Once delim reached, push split into split string
+		if (c == delim) {
+			arr[i] = currentSplit;
+			currentSplit = "";
+			++i;
+		// If not delim, add char to current split
+		} else {
+			currentSplit += c;
+		}
+	}
+
+	// Append the remaining string if not blank
+	arr[i] = currentSplit;
+}
+
 // Parses input definition from given line
 void Compiler::parseInput(string line) {
-	// Create regex for parsing input definitions
+	// Regex for parsing input definitions
 	regex parseInputRegex(INPUT_TYPE "\\s+(\\w*)\\s+\"(.*?)\"");
 
 	// Will hold regex matches
 	smatch matches;
 
-	// Research line for above regex to parse input
+	// Search line with regex
 	if (!regex_search(line, matches, parseInputRegex)) return;
 
 	// Parse input name and the input string from the regex
@@ -31,7 +71,49 @@ void Compiler::parseInput(string line) {
 
 // Parses state definition from given line
 void Compiler::parseState(string line) {
+	// Regex for parsing state definitions
+	regex parseStateRegex(STATE_TYPE "\\s+(\\w*)\\s+\\[(.*?)\\]");
 
+	// Will hold regex matches
+	smatch matches;
+
+	// Search line with regex
+	if (!regex_search(line, matches, parseStateRegex)) return;
+
+	// Parse input name and raw transition mapping string
+	string stateName = matches.str(1);
+	string rawTransitionMap = matches.str(2);
+
+	int transitionMapSplitSize = count(rawTransitionMap.begin(), rawTransitionMap.end(), TRANSITION_SEPARATOR) + 1;
+
+	// Create string array to hold split transition map
+	string* transitionMapSplit = new string[transitionMapSplitSize];
+
+	// Split transition map at commas
+	split(rawTransitionMap, TRANSITION_SEPARATOR, transitionMapSplit);
+
+	// Maps input names to state names; represents transitions
+	map<string, string> transitionMap;
+
+	// Current transition being parsed in loop
+	string transition[2];
+
+	// Parse transition mappings from raw transition map
+	for (int i = 0; i < transitionMapSplitSize; ++i) {
+		// Check that split won't cause out of bounds access
+		if (count(transitionMapSplit[i].begin(), transitionMapSplit[i].end(), MAPPING_SEPARATOR) != 1) {
+			return;
+		}
+
+		// Parse transition and add it to the map
+		split(transitionMapSplit[i], MAPPING_SEPARATOR, transition);
+		transitionMap[trim(transition[0])] = trim(transition[1]);
+	}
+
+	// Add this state and its corresponding transitions to the states map
+	states[stateName] = transitionMap;
+
+	delete[] transitionMapSplit;
 }
 
 // Parses other statements, such as built-in functions
@@ -46,8 +128,6 @@ void Compiler::parse() {
 
 	// Iterate through lines in source
 	while (getline(src, line)) {
-		printf("%s\n", line.c_str());
-
 		// Check what the line is doing. Call appropriate function to parse it
 		if (line.find(INPUT_TYPE) != string::npos) parseInput(line);
 		else if (line.find(STATE_TYPE) != string::npos) parseState(line);
--- src/compiler.h
+++ src/compiler.h
@@ -4,17 +4,21 @@
 // Keywords
 #define INPUT_TYPE "INPUT"
 #define STATE_TYPE "STATE"
+#define TRANSITION_SEPARATOR ','
+#define MAPPING_SEPARATOR ':'
 #define MAP_OPEN "["
 #define MAP_CLOSE "]"
 #define BODY_START "{"
 #define BODY_END "}"
 
 #include <cstdio>
+#include <cctype>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <regex>
+#include <algorithm>
 #include <map>
 
 using namespace std;
@@ -25,6 +29,9 @@ private:
 	ifstream src;								// Source code file
 	map<string, string> inputs;					// Maps input name to input string
 	map<string, map<string, string>> states;	// States and input -> state mappings
+
+	static string trim(string);
+	static void split(string, char, string*);
 
 public:
 	Compiler(string);
