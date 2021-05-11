8b444763f18c4f75bb2f2acb8f0ede9085036901
Jack Sheehan <jaxanimations@gmail.com>
2021-05-10 23:41:54 -0500

    Implemented basic state logic compilation

--- src/writer.cpp
+++ src/writer.cpp
@@ -1,6 +1,6 @@
 #include "writer.h"
 
-// Takes path to target file and pointers to inputs and states
+// Takes path to target file and pointers to inputs and states. Also takes size of input C string array
 Writer::Writer(string p, map<string, string>* i, map<string, map<string, string>>* s) {
 	f.open(p);
 	inputs = i;
@@ -12,14 +12,19 @@ Writer::~Writer() {
 	f.close();
 }
 
-// Declares enum for alphabet in target file
+// Writes imports needed in compiled code
+void Writer::writeIncludes() {
+	f << "#include<cstdio>\n"
+	  << "#include<string>\n"
+	  << "using namespace std;\n";
+}
+
+// Declares constants for alphabet in target file
 void Writer::declareAlphabet() {
 	// Write enum for input alphabet
-	f << "enum InputAlphabet {\n";
 	for (auto& input : *inputs) {
-		f << "\t" << input.first << " = \"" << input.second << "\",\n";
+		f << "const string " << input.first << " = \"" << input.second << "\";\n";
 	}
-	f << "};\n";
 }
 
 // Declares enum for states in target file
@@ -32,15 +37,47 @@ void Writer::declareStates() {
 	f << "};\n";
 }
 
-
 // Writes main function and state change logic
 void Writer::writeLogic() {
 	// Write beginning of main function
 	f << "int main() {\n";
+
+	// Declare input variable, starting state, while loop opening, and switch head
+	f << "\tchar in[" << INPUT_SIZE << "];\n"
+	  << "\tState state = " << states->begin()->first << ";\n"
+	  << "\twhile(1) {\n"
+	  << "\t\tswitch(state) {\n";
+
+	// Flag to determine if "if" should be written
+	bool writeIf;
+
+	// Write case for each state
+	for (auto& state : *states) {
+		f << "\t\tcase " << state.first << ":\n";
+
+		writeIf = true;
+
+		// Write if/else for each transition to change state
+		for (auto& trans : state.second) {
+			// Write if/else if branch
+			if (writeIf) {
+				f << "\t\t\tif(";
+				writeIf =  false;
+			} else {
+				f<< "\t\t\telse if(";
+			}
+
+			// Write state transition logic
+			f << "in == " << trans.first << ") state = " << trans.second << ";\n";
+		}
+	}
+	// Close switch, while loop, and main
+	f << "\t\t}\n\t}\n}";
 }
 
 // Function to drive helper functions to compile to target language
 void Writer::write() {
+	writeIncludes();
 	declareAlphabet();
 	declareStates();
 	writeLogic();
